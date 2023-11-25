b4697c66240916d654c72a3719e0d6cc469e7121
Muhammad Yusuf <yusuf@iamyuu.dev>
2023-11-25 17:58:14 +0700

    feat: add movie list provider

---
+++ lib/presentation/providers/movie/now_playing_provider.dart
@@ -0,0 +1,35 @@
+import 'package:flix_tix/domain/entities/movie.dart';
+import 'package:flix_tix/domain/entities/result.dart';
+import 'package:flix_tix/domain/usecases/get_movie_list/get_movie_list.dart';
+import 'package:flix_tix/presentation/providers/usecase/get_movie_list_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'now_playing_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+class NowPlaying extends _$NowPlaying {
+  @override
+  FutureOr<List<Movie>> build() => const [];
+
+  Future<void> getMovies({int page = 1}) async {
+    state = const AsyncLoading();
+
+    GetMovieList getMovieList = ref.read(getMovieListProvider);
+
+    final result = await getMovieList(
+      GetMovieListParams(
+        page: page,
+        type: MovieListType.nowPlaying,
+      ),
+    );
+
+    switch (result) {
+      case Success(value: final movies):
+        state = AsyncData(movies);
+
+      case Error(error: _):
+        state = const AsyncData([]);
+        break;
+    }
+  }
+}
