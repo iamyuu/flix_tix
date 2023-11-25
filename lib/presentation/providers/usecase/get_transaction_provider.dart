42a9d66f85b250575425a792180671900dd4a977
Muhammad Yusuf <yusuf@iamyuu.dev>
2023-11-25 17:00:09 +0700

    feat: add transaction providers

---
+++ lib/presentation/providers/usecase/get_transaction_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flix_tix/domain/usecases/get_transaction/get_transaction.dart';
+import 'package:flix_tix/presentation/providers/repositories/transaction_repository/transaction_repository_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'get_transaction_provider.g.dart';
+
+@riverpod
+GetTransaction getTransaction(GetTransactionRef ref) => GetTransaction(
+      transactionRepository: ref.watch(transactionRepositoryProvider),
+    );
